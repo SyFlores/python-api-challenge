2522f8f0b3614e9eb48af57f11dafc4e09d0588c
SyFlores <syestevenflores@gmail.com>
2021-04-17 15:47:07 -0700

    Checking if git ignore works.

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Your OpenWeatherAPI key here"
+weather_api_key = "b3617f38c1c51841b876146c0e4f2f11"
 
 # Google API Key
-g_key = "Your Google API key here"
+g_key = "AIzaSyB0OraBol8QzxDNVZryivHzwR4H7G_6a_s"
